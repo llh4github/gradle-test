9cafad787dc3be785aa3c8e641065a035d3894c7
llh <lilinhong_coding@foxmail.com>
2022-11-05 16:48:22 +0800

    fix: 包位置重构

---
+++ lib/src/main/kotlin/ll4github/demo/Hello.kt
@@ -0,0 +1,15 @@
+package ll4github.demo
+
+/**
+ *
+ *
+ * Created At 2022/11/5 16:47
+ * @author llh
+ */
+class Hello {
+    companion object {
+        fun println() {
+            println("Hello ...")
+        }
+    }
+}
